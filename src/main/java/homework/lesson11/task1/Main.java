fe91db3ce10bee84ab46b62b701540b219762ab1
Ostapiak Mykhailo <ostapiakmz@gmail.com>
2026-06-20 17:30:16 +0300

    Fix: shut down ExecutorService

--- src/main/java/homework/lesson11/task1/Main.java
+++ src/main/java/homework/lesson11/task1/Main.java
@@ -2,16 +2,20 @@ package homework.lesson11.task1;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
 
+        ExecutorService executor = Executors.newFixedThreadPool(10);
+
         for (int i = 0; i < 10; i++) {
-            ExecutorService executor = Executors.newFixedThreadPool(10);
             executor.submit(new Counter());
-            executor.close();
         }
+        executor.shutdown();
+        executor.awaitTermination(5, TimeUnit.SECONDS);
+
         System.out.println(Counter.COUNTER);
     }
 }
