9b2998a465abea07c1b918f07f42d0b6af8e5dd4
Ostapiak Mykhailo <ostapiakmz@gmail.com>
2026-07-01 21:13:15 +0300

    Refactor: employee creation logic

--- src/main/java/homework/lesson12/EmployeeCreator.java
+++ src/main/java/homework/lesson12/EmployeeCreator.java
@@ -3,6 +3,7 @@ package homework.lesson12;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class EmployeeCreator {
 
@@ -16,53 +17,33 @@ public class EmployeeCreator {
 
     public Employee createEmployee() throws IOException {
 
-        String name = getValidName();
-        String email = getValidEmail();
-        String number = getValidNumber();
-        int salary = getValidSalary();
+        String name = getValidInput("Please enter employee`s name or enter `0` to omit this step", true);
+        if (name != null) {
+            validator.validateName(name);
+        }
+
+        String email = getValidInput("Enter employee`s email or enter `0` to omit this step", true);
+        if (email != null) {
+            validator.validateEmail(email);
+        }
+
+        String number = getValidInput("Please enter employee`s number", false);
+        validator.validateNumber(number);
+
+        int salary = Integer.parseInt(Objects.requireNonNull(getValidInput("What salary does employee get?", false)));
+        validator.validateSalary(salary);
 
         Employee employee = new Employee(name, email, number, salary);
         employees.add(employee);
         return employee;
     }
 
-    private int getValidSalary() throws IOException {
-        System.out.println("What salary does employee get?");
-        int salary;
-        try {
-            salary = Integer.parseInt(reader.getInput());
-        } catch (NumberFormatException e) {
-            throw new ValidationException("This field must be a number");
+    private String getValidInput(String message, boolean allowNull) throws IOException {
+        System.out.println(message);
+        String input = reader.getInput();
+        if (allowNull && "0".equals(input)) {
+            input = null;
         }
-        return salary;
-    }
-
-    private String getValidNumber() throws IOException {
-        System.out.println("Please enter employee`s number");
-        String number = reader.getInput();
-        validator.validateNumber(number);
-        return number;
-    }
-
-    private String getValidEmail() throws IOException {
-        System.out.println("Enter employee`s email or enter `0` to omit this step");
-        String email = reader.getInput();
-        if ("0".equals(email)) {
-            email = null;
-        } else {
-            validator.validateEmail(email);
-        }
-        return email;
-    }
-
-    private String getValidName() throws IOException {
-        System.out.println("Please enter employee`s name or enter `0` to omit this step");
-        String name = reader.getInput();
-        if ("0".equals(name)) {
-            name = null;
-        } else {
-            validator.validateName(name);
-        }
-        return name;
+        return input;
     }
 }
