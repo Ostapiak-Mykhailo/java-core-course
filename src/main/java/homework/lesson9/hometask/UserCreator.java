bd167cce53d46a42620866ab8ff525ad3cce0544
Ostapiak Mykhailo <ostapiakmz@gmail.com>
2026-07-13 20:31:32 +0300

    refactor: change message passing logic

--- src/main/java/homework/lesson9/hometask/UserCreator.java
+++ src/main/java/homework/lesson9/hometask/UserCreator.java
@@ -9,27 +9,22 @@ public class UserCreator {
     private ConsoleReader reader = new ConsoleReader();
     private Validator validator;
     private final List<User> users = new ArrayList<>();
-    private final String namePrompt = "Welcome to our web site. Please enter your name to check in";
-    private final String emailPrompt = "Enter your Email";
-    private final String numberPrompt = "Enter your phone number";
-    private final String passwordPrompt = "Come up with strong password";
-    private final String confirmPasswordPrompt = "Confirm your password";
 
     public UserCreator(Validator validator) {
         this.validator = validator;
     }
 
     public void runApp() throws IOException {
-        String name = getValidInput(namePrompt);
+        String name = getValidInput("Welcome to our web site. Please enter your name to check in");
         validator.validateName(name);
 
-        String email = getValidInput(emailPrompt);
+        String email = getValidInput("Enter your Email");
         validator.validateEmail(email);
 
-        String number = getValidInput(numberPrompt);
+        String number = getValidInput("Enter your phone number");
         validator.validatePhoneNumber(number);
 
-        String password = getValidInput(passwordPrompt);
+        String password = getValidInput("Come up with strong password");
         validator.validatePassWord(password);
 
         if (!confirmPassword(password)) {
@@ -42,7 +37,7 @@ public class UserCreator {
     }
 
     private boolean confirmPassword(String password) throws IOException {
-        return password.equals(getValidInput(confirmPasswordPrompt));
+        return password.equals(getValidInput("Confirm your password"));
     }
 
     private String getValidInput(String message) throws IOException {
