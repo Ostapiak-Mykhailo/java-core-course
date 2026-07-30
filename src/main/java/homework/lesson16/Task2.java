25bf127bc62ea11a3c15e3aa6787ca535f84bc98
Ostapiak Mykhailo <ostapiakmz@gmail.com>
2026-07-30 22:24:04 +0300

    Add: stream and map solution

--- src/main/java/homework/lesson16/Task2.java
+++ src/main/java/homework/lesson16/Task2.java
@@ -1,24 +1,37 @@
 package homework.lesson16;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class Task2 {
 
     public static void main(String[] args) {
-        String text1 = "254832";
-        String text2 = "2548354756535";
+        String text1 = "f , 25fff4832";
+        String text2 = "25483ggggggg54756535";
         String text3 = "254835";
 
         System.out.println(ifNumberNrepeatedNTimes(text1));
         System.out.println(ifNumberNrepeatedNTimes(text2));
         System.out.println(ifNumberNrepeatedNTimes(text3));
+
+        System.out.println();
+
+        System.out.println(ifNumberNrepeatedNTimesByMap(text1));
+        System.out.println(ifNumberNrepeatedNTimesByMap(text2));
+        System.out.println(ifNumberNrepeatedNTimesByMap(text3));
+
+        System.out.println();
+
+        System.out.println(ifNumberNrepeatedNTimesByStream(text1));
+        System.out.println(ifNumberNrepeatedNTimesByStream(text2));
+        System.out.println(ifNumberNrepeatedNTimesByStream(text3));
     }
 
     private static boolean ifNumberNrepeatedNTimes(String string) {
-        if (!validateInput(string)){
-            throw new IllegalArgumentException();
-        }
+        string = validateInput(string);
         String[] strNumbers = string.split("");
         int[] numbers = new int[strNumbers.length];
 
@@ -40,9 +53,31 @@ public class Task2 {
         return false;
     }
 
-    private static boolean validateInput(String s){
-         Pattern pattern = Pattern.compile("[2-9]+");
-        Matcher matcher = pattern.matcher(s);
-        return matcher.matches();
+    private static boolean ifNumberNrepeatedNTimesByMap(String string) {
+        string = validateInput(string);
+        String[] strNumbers = string.split("");
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (String strNumber : strNumbers) {
+            int number = Integer.parseInt(strNumber);
+            map.merge(number, 1, Integer::sum);
+        }
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getKey().equals(entry.getValue())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static boolean ifNumberNrepeatedNTimesByStream(String string) {
+        string = validateInput(string);
+        return string.chars().map(a -> a - '0').boxed().collect(Collectors
+                        .groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet().stream().anyMatch(e -> e.getKey().equals(e.getValue().intValue()));
+    }
+
+    private static String validateInput(String s) {
+        return s.replaceAll("\\D", "");
     }
 }
